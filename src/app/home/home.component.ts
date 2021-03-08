6d13a6ed498231cc15e989317a885254a424e406
vinicius.teixeira <vinicius.teixeira@mmtools.com.br>
2021-03-08 13:01:24 -0300

    First Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { SkinsService } from '../services/skins.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private SkinsService: SkinsService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
